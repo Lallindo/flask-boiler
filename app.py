e03b9cf3b772f9dad444d5a09b495799c8723e9a
Lallindo <brunolallo8@gmail.com@>
2025-05-30 08:12:05 -0300

    feat: Redirect to Item page when url has no path

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from views import create_app
 
 app = create_app()
 
+@app.get('/')
+def redirect_to_home():
+    return redirect(url_for("item_bp.items"))
+
 if __name__ == '__main__':
     app.run(debug=True)
--- exceptions.py
+++ exceptions.py
@@ -2,7 +2,7 @@ from werkzeug.exceptions import HTTPException
 
 class CustomError(HTTPException):
     code = 418
-    description = "Um erro genérico da minha API ocorreu."
+    description = "Um erro genérico ocorreu."
 
     def __init__(self, description=None, response=None, custom_data=None):
         super().__init__(description, response)
